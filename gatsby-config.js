e12ff9a5f72e6d45a3cd06870b0d2aa099b04bc2
Dan Cooper <cooperda.5@gmail.com>
2019-06-10 15:09:07 +0700

    adds site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,11 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Peru Tour",
+    description:
+      "Explore life-changing tours in the beautiful country of Peru. Break out of your daily routine and experience something special!",
+    author: "cooperda",
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
